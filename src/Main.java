20e53777f49e22d75f73587f50d70c6b49b93839
Yves PIQUET <Yves@MacBook-ysp.lan>
2019-10-23 15:23:48 +0200

    Debut qui fonctionne

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 import java.util.Random;
 
 public class Main {
-	
+	private static Random rand = new Random();
+
 	public static int randInt(int min, int max) {
-		Random rand = new Random();
 		int randNum = rand.nextInt((max - min) +1) + min;
 		return randNum;
 	}
 	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println();
-	
-		}
+	public static void main(String[] args) 
+	{
+		Sterckets jeanjean = new Sterckets("Jean");
+		jeanjean.affiche();
+		System.out.println(jeanjean.description());
+	}
 		
 		
 		
--- src/Sterckets.java
+++ src/Sterckets.java
@@ -9,35 +9,93 @@ public class Sterckets
 	private int def;
 	private int rang;
 	private String element;
-	private int points;
 
 	
 	//methode 
 	public Sterckets(String name){
+		int points;
+		points = points();
 		this.name = name;
-		this.pv = 0;
-		this.att = 0;
-		this.def = 0;
+		this.pv = Main.randInt(1, points-2);
+		this.att = Main.randInt(1,(points - pv - 1));
+		this.def = points - pv - att;
 		this.rang = Main.randInt(1,3);
-		this.element = "feu";
-		this.points = points();
+		this.element = elem();
+		
 	}
 	
 	//methodes
 	
-	public int points() {
+	private int points() 
+	{
 		int pointPerso = Main.randInt(10, 100);
-		if(this.rang == 2) {
-			return (pointPerso * 10)/100;
+		if(this.rang == 2) 
+		{
+			pointPerso = ((pointPerso * 10)/100) + pointPerso;
+			if (pointPerso > 100)
+			{
+				pointPerso = 100;
+				return pointPerso;
+			}
+			return pointPerso;
 		}
-		if(this.rang == 3) {
-			return (pointPerso * 20)/100;
+		if(this.rang == 3) 
+		{
+			pointPerso = ((pointPerso * 20)/100) + pointPerso;
+			if (pointPerso > 100)
+			{
+				pointPerso = 100;
+				return pointPerso;
+			}
+			return pointPerso;
 		}
 		return pointPerso;
-		
 	}
+
 	
 	
+	private String elem()
+	{
+		int num = Main.randInt(1,4);
+
+		switch (num)
+		{
+			case 1 :
+				return "Feu";
+			case 2 :
+				return "Eau";
+			case 3 :
+				return "Terre";
+			case 4 :
+				return "Air";
+			default :
+				return "Should never happen";
+		}
+		/*if (num == 1)
+			{
+				return "feu";
+			}
+		if (num == 2)
+		{
+			return "Eau";
+		}
+		if (num == 3)
+		{
+			return "Terre";
+		}
+		return "Air";
+		*/
+	}
+
+	public void affiche()
+	{
+		System.out.println(name + "(" + element + ")" + " rang " + rang + " " + pv + " pv");
+	}
+
+	public String description()
+	{
+		return name + "(" + element + ")" + " rang " + rang + " " + pv + " pv";
+	}
 	
 
 }
